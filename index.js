931d30bb3ae1421c19531af99cf114a1f18a2d04
Helka Lantto <hmlantto@gmail.com>
2022-02-17 13:39:29 +0200

    Commit up to exercise 13.8

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const { PORT } = require('./utils/config')
 const { connectToDatabase } = require('./utils/db')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 const errorHandler = (error, req, res, next) => {
   console.error(error.message)
